af162c1f9be9cc78b3f45315a051b2b76dd98492
Colin O'Dell <colinodell@gmail.com>
2011-09-23 12:33:33 -0400

    Changed application name and pname; added an Exit button; resized a couple images

--- src/com/samsung/downloadtestapp/CongratsActivity.java
+++ src/com/samsung/downloadtestapp/CongratsActivity.java
@@ -1,8 +1,10 @@
-package com.colinodell.samsung.marketdownloadtest;
+package com.samsung.downloadtestapp;
 
 import android.app.Activity;
 import android.graphics.PixelFormat;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
 
 public class CongratsActivity extends Activity {
@@ -11,6 +13,13 @@ public class CongratsActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        this.findViewById(R.id.close).setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View arg0) {
+				CongratsActivity.this.finish();
+			}});
     }
     
     @Override
